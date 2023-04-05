cfcbf1afc5bc17691036fe34b6212018deb050d7
RoqayaAlmamari <roqayaalmamari@gmail.com>
2023-04-05 09:31:53 +0400

    channge student class

--- src/main/java/com/example/IMS_UI_API/model/Student.java
+++ src/main/java/com/example/IMS_UI_API/model/Student.java
@@ -4,59 +4,17 @@ package com.example.IMS_UI_API.model;
  The Student class represents a student in the Institute Management System.
  */
 public class Student {
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String email;
+    public int id;
+    public String name;
 
-    private int phone;
+    public String email;
 
-    public Student(int id, String firstName, String lastName, String email, int phone) {
+    public int phone;
+
+    public Student(int id, String name, String email, int phone) {
         this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.name = name;
         this.email = email;
-        this.phone = phone;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPhone() {
-        return phone;
-    }
-
-    public void setPhone(int phone) {
         this.phone = phone;
     }
 }
-
